b3d8b4847ce71eb36d979e01c520f1304fadf3a5
takapi-Jr <tsoda@hotmail.co.jp>
2019-02-02 18:01:37 +0900

    MultiTriggerConverterクラスをConvertersフォルダへ移動。

--- FaceAPI_SampleApp/FaceAPI_SampleApp/FaceAPI_SampleApp/ViewModels/SettingPageViewModel.cs
+++ FaceAPI_SampleApp/FaceAPI_SampleApp/FaceAPI_SampleApp/ViewModels/SettingPageViewModel.cs
@@ -95,25 +95,4 @@ namespace FaceAPI_SampleApp.ViewModels
             
         }
     }
-
-    /// <summary>
-    /// MultiTrigger使用時の文字列の長さ比較用クラス
-    /// </summary>
-    public class MultiTriggerConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType,
-            object parameter, CultureInfo culture)
-        {
-            if ((int)value > 0)         // length > 0 ?
-                return true;            // some data has been entered
-            else
-                return false;           // input is empty
-        }
-
-        public object ConvertBack(object value, Type targetType,
-            object parameter, CultureInfo culture)
-        {
-            throw new NotSupportedException();
-        }
-    }
 }
